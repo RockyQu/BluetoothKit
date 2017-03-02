d9802cc1f6cf679c51bd6d730f0bd3a467730891
DesignQu <250533855@qq.com>
2017-03-02 11:00:22 +0800

    Initial commit

---
+++ App/src/main/java/com/tool/ibeacon/manager/example/DeviceAdapter.java
@@ -0,0 +1,136 @@
+package com.tool.ibeacon.manager.example;
+
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.content.res.Resources;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.tool.ibeacon.manager.util.DateUtil;
+import com.tool.ibeacon.manager.util.ScannedDevice;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ *
+ */
+public class DeviceAdapter extends ArrayAdapter<ScannedDevice> {
+    private static final String PREFIX_RSSI = "RSSI:";
+    private static final String PREFIX_LASTUPDATED = "Last Udpated:";
+    private List<ScannedDevice> mList;
+    private LayoutInflater mInflater;
+    private int mResId;
+
+    public DeviceAdapter(Context context, int resId, List<ScannedDevice> objects) {
+        super(context, resId, objects);
+        mResId = resId;
+        mList = objects;
+        mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ScannedDevice item = (ScannedDevice) getItem(position);
+
+        if (convertView == null) {
+            convertView = mInflater.inflate(mResId, null);
+        }
+        TextView name = (TextView) convertView.findViewById(R.id.device_name);
+        name.setText(item.getDisplayName());
+        TextView address = (TextView) convertView.findViewById(R.id.device_address);
+        address.setText(item.getDevice().getAddress());
+        TextView rssi = (TextView) convertView.findViewById(R.id.device_rssi);
+        rssi.setText(PREFIX_RSSI + Integer.toString(item.getRssi()));
+        TextView lastupdated = (TextView) convertView.findViewById(R.id.device_lastupdated);
+        lastupdated.setText(PREFIX_LASTUPDATED + DateUtil.get_yyyyMMddHHmmssSSS(item.getLastUpdatedMs()));
+
+        TextView ibeaconInfo = (TextView) convertView.findViewById(R.id.device_ibeacon_info);
+        Resources res = convertView.getContext().getResources();
+        if (item.getIBeacon() != null) {
+            ibeaconInfo.setText(res.getString(R.string.label_ibeacon) + "\n"
+                    + item.getIBeacon().toString());
+        } else {
+            ibeaconInfo.setText(res.getString(R.string.label_not_ibeacon));
+        }
+        TextView scanRecord = (TextView) convertView.findViewById(R.id.device_scanrecord);
+        scanRecord.setText(item.getScanRecordHexString());
+
+        return convertView;
+    }
+
+    /**
+     * add or update BluetoothDevice List
+     * 
+     * @param newDevice Scanned Bluetooth Device
+     * @param rssi RSSI
+     * @param scanRecord advertise data
+     * @return summary ex. "iBeacon:3 (Total:10)"
+     */
+    public String update(BluetoothDevice newDevice, int rssi, byte[] scanRecord) {
+        if ((newDevice == null) || (newDevice.getAddress() == null)) {
+            return "";
+        }
+        long now = System.currentTimeMillis();
+
+        boolean contains = false;
+        for (ScannedDevice device : mList) {
+            if (newDevice.getAddress().equals(device.getDevice().getAddress())) {
+                contains = true;
+                // update
+                device.setRssi(rssi);
+                device.setLastUpdatedMs(now);
+                device.setScanRecord(scanRecord);
+                break;
+            }
+        }
+        if (!contains) {
+            // add new BluetoothDevice
+            mList.add(new ScannedDevice(newDevice, rssi, scanRecord, now));
+        }
+
+        // sort by RSSI
+        Collections.sort(mList, new Comparator<ScannedDevice>() {
+            @Override
+            public int compare(ScannedDevice lhs, ScannedDevice rhs) {
+                if (lhs.getRssi() == 0) {
+                    return 1;
+                } else if (rhs.getRssi() == 0) {
+                    return -1;
+                }
+                if (lhs.getRssi() > rhs.getRssi()) {
+                    return -1;
+                } else if (lhs.getRssi() < rhs.getRssi()) {
+                    return 1;
+                }
+                return 0;
+            }
+        });
+
+        notifyDataSetChanged();
+
+        // create summary
+        int totalCount = 0;
+        int iBeaconCount = 0;
+        if (mList != null) {
+            totalCount = mList.size();
+            for (ScannedDevice device : mList) {
+                if (device.getIBeacon() != null) {
+                    iBeaconCount++;
+                }
+            }
+        }
+        String summary = "iBeacon:" + Integer.toString(iBeaconCount) + " (Total:"
+                + Integer.toString(totalCount) + ")";
+
+        return summary;
+    }
+    
+    public List<ScannedDevice> getList() {
+        return mList;
+    }
+}
---
+++ App/src/main/java/com/tool/ibeacon/manager/example/ScanActivity.java
@@ -0,0 +1,158 @@
+package com.tool.ibeacon.manager.example;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.widget.ListView;
+import android.widget.Toast;
+import com.tool.ibeacon.manager.util.BleUtil;
+import com.tool.ibeacon.manager.util.ScannedDevice;
+
+import java.util.ArrayList;
+
+/**
+ *
+ */
+public class ScanActivity extends Activity implements BluetoothAdapter.LeScanCallback {
+
+    private BluetoothAdapter mBTAdapter;
+    private DeviceAdapter mDeviceAdapter;
+    private boolean mIsScanning;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+        setContentView(R.layout.activity_scan);
+
+        init();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if ((mBTAdapter != null) && (!mBTAdapter.isEnabled())) {
+            Toast.makeText(this, R.string.bt_not_enabled, Toast.LENGTH_SHORT).show();
+            invalidateOptionsMenu();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        stopScan();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        if (mIsScanning) {
+            menu.findItem(R.id.action_scan).setVisible(false);
+            menu.findItem(R.id.action_stop).setVisible(true);
+        } else {
+            menu.findItem(R.id.action_scan).setEnabled(true);
+            menu.findItem(R.id.action_scan).setVisible(true);
+            menu.findItem(R.id.action_stop).setVisible(false);
+        }
+        if ((mBTAdapter == null) || (!mBTAdapter.isEnabled())) {
+            menu.findItem(R.id.action_scan).setEnabled(false);
+        }
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == android.R.id.home) {
+            // ignore
+            return true;
+        } else if (itemId == R.id.action_scan) {
+            startScan();
+            return true;
+        } else if (itemId == R.id.action_stop) {
+            stopScan();
+            return true;
+        } else if (itemId == R.id.action_clear) {
+            if ((mDeviceAdapter != null) && (mDeviceAdapter.getCount() > 0)) {
+                mDeviceAdapter.clear();
+                mDeviceAdapter.notifyDataSetChanged();
+                getActionBar().setSubtitle("");
+            }
+            return true;
+        }
+        return super.onMenuItemSelected(featureId, item);
+    }
+
+    @Override
+    public void onLeScan(final BluetoothDevice newDeivce, final int newRssi,
+                         final byte[] newScanRecord) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                String summary = mDeviceAdapter.update(newDeivce, newRssi, newScanRecord);
+                if (summary != null) {
+                    getActionBar().setSubtitle(summary);
+                }
+            }
+        });
+    }
+
+    private void init() {
+        // BLE check
+        if (!BleUtil.isBLESupported(this)) {
+            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+
+        // BT check
+        BluetoothManager manager = BleUtil.getManager(this);
+        if (manager != null) {
+            mBTAdapter = manager.getAdapter();
+        }
+        if (mBTAdapter == null) {
+            Toast.makeText(this, R.string.bt_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+
+        // init listview
+        ListView deviceListView = (ListView) findViewById(R.id.list);
+        mDeviceAdapter = new DeviceAdapter(this, R.layout.listitem_device,
+                new ArrayList<ScannedDevice>());
+        deviceListView.setAdapter(mDeviceAdapter);
+        stopScan();
+    }
+
+    private void startScan() {
+        if ((mBTAdapter != null) && (!mIsScanning)) {
+            mBTAdapter.startLeScan(this);
+            mIsScanning = true;
+            setProgressBarIndeterminateVisibility(true);
+            invalidateOptionsMenu();
+        }
+    }
+
+    private void stopScan() {
+        if (mBTAdapter != null) {
+            mBTAdapter.stopLeScan(this);
+        }
+        mIsScanning = false;
+        setProgressBarIndeterminateVisibility(false);
+        invalidateOptionsMenu();
+    }
+
+}
