552c5eb619c2fade448ef81c31d8f092598c4fe4
DesignQu <250533855@qq.com>
2017-11-28 16:49:35 +0800

    去掉无用类

--- App/src/main/java/com/tool/ibeacon/manager/example/ScanActivity.java
+++ App/src/main/java/com/tool/ibeacon/manager/example/ScanActivity.java
@@ -27,7 +27,6 @@ public class ScanActivity extends Activity implements BluetoothAdapter.LeScanCal
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_scan);
 
         init();
